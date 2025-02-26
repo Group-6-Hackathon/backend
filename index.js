85916d933bdf083fd34c6b952df31184be37bd5d
Destin Saba <129232125+destinsaba@users.noreply.github.com>
2025-02-25 17:34:51 -0700

    MongoDB connection setup, added dotenv and mongoose packages

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const port = 3000;
 const database = require('./db');
 const routes = require('./routes/route');
 
-database();
+async function startServer() {
+    await database();
 
-app.use(express.json());
-// app.use('', routes); // to include when route is created
+    app.use(express.json());
+    // app.use('', routes); // to include when route is created
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+}
+
+startServer();
